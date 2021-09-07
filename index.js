b2aaa2db3055b2d5ef198b3125b24b1f6fb0632b
Aminur Rahman <90109513+araminur@users.noreply.github.com>
2021-09-07 13:54:07 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+let admission = document.getElementById("admission");
+
+admission.addEventListener('click',()=>{
+    alert(`Admission coming soon`);
+});
+
+function myFunction(){
+    alert(`Admission coming soon`);
+};
